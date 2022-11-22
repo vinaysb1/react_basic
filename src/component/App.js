0153657c896be6fdef3aa53ef108e94d28f7107b
vinay <vinaykumarbeelagi@gmail.com>
2022-11-22 20:01:19 +0530

    contact list is added

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Addcontact from './Addcontact';
 
 function App() {
   return (
     <>
     <div className='ui conatiner'> 
     <Header/>
+    <Addcontact/>
     </div>
     </>
   );
